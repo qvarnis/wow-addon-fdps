a13473fb1a5c6bd78e404b380dcd33c529d29550
jqvar <jan.qvarnstrom@su.se>
2014-12-22 11:53:56 +0100

    Fixed to working state

--- fdps.lua
+++ fdps.lua
@@ -15,3 +15,21 @@ local button = CreateFrame('Button', name, nil, 'SecureActionButtonTemplate,Secu
 button:SetAttribute('type', 'macro')
 button:SetAttribute('macro',macroName)
 button:SetScript("PostClick",UpdateIcon)
+
+-- local frame = CreateFrame("Frame", "TestFrame", UIParent)
+local frame = CreateFrame('Button', "TestFrame", UIParent,'SecureActionButtonTemplate,SecureHandlerBaseTemplate')
+local background = frame:CreateTexture("TestFrameBackground", "BACKGROUND")
+background:SetTexture(1, 1, 1, 0.25)
+background:SetAllPoints()
+
+-- Set the top left corner 5px to the right and 15px above UIParent's top left corner
+frame:SetPoint("TOPLEFT", 5, 15)
+
+-- Set the bottom edge to be 10px below WorldFrame's center
+frame:SetPoint("BOTTOM", WorldFrame, "CENTER", 0, -10)
+
+-- Set the right edge to be 20px to the left of WorldFrame's right edge
+frame:SetPoint("RIGHT", WorldFrame, -20, 0)
+
+frame:SetAttribute('type', 'macro')
+frame:SetAttribute('macro',macroName)
